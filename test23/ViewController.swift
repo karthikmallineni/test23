467a592f0cb77311fef4032af5cc6c9bc3e236bf
Karthik <mkarthik.appdev@gmail.com>
2020-08-02 23:39:52 +0530

    Initial Commit

---
+++ test23/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test23
+//
+//  Created by Administrator on 02/08/20.
+//  Copyright © 2020 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
