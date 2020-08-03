4f325c078377a438e02e3153266e84d9bf28d8bd
Karthik <mkarthik.appdev@gmail.com>
2020-08-03 11:49:22 +0530

    only second

---
+++ test23/secondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  secondViewController.swift
+//  test23
+//
+//  Created by Karthik on 03/08/20.
+//  Copyright © 2020 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var name = "iegre"
+        print(name)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
